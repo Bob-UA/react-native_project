049b214175ab9fdaad8f09c90823f403379d822e
Bob-UA <120073426+Bob-UA@users.noreply.github.com>
2023-10-07 10:05:03 -0700

    add components for keyboard

--- Screens/LoginScreen.jsx
+++ Screens/LoginScreen.jsx
@@ -1,17 +1,19 @@
 import { useEffect, useState } from "react";
 import {
   ImageBackground,
+  Keyboard,
   Pressable,
   Text,
   TextInput,
+  TouchableWithoutFeedback,
   View,
 } from "react-native";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { styles } from "./styles";
 
 const LoginScreen = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, onChangeTextEmail] = useState("");
+  const [password, onChangeTextPassword] = useState("");
   const [showPassword, setShowPassword] = useState(false);
 
   const toggleShowPassword = () => {
@@ -38,45 +40,47 @@ const LoginScreen = () => {
         source={require("../src/img/photo_bg.jpg")}
         style={styles.image}
       >
-        <View style={styles.loginContainer}>
-          <Text style={styles.titleText}>Login</Text>
-          <TextInput
-            style={styles.emailInput}
-            placeholder="Email"
-            type="email"
-            name="email"
-            value={email}
-            required
-            onChange={setEmail}
-          />
-          <View style={styles.passwordContainer}>
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <View style={styles.loginContainer}>
+            <Text style={styles.titleText}>Login</Text>
             <TextInput
-              style={styles.input}
-              secureTextEntry={!showPassword}
-              placeholder="Password"
-              type="password"
-              name="password"
-              value={password}
+              style={styles.emailInput}
+              placeholder="Email"
+              type="email"
+              name="email"
+              value={email}
               required
-              onChange={setPassword}
+              onChangeText={onChangeTextEmail}
             />
-            <MaterialCommunityIcons
-              name={showPassword ? "eye-off" : "eye"}
-              size={24}
-              color="#aaa"
-              style={styles.toggle}
-              onPress={toggleShowPassword}
-            />
+            <View style={styles.passwordContainer}>
+              <TextInput
+                style={styles.input}
+                secureTextEntry={!showPassword}
+                placeholder="Password"
+                type="password"
+                name="password"
+                value={password}
+                required
+                onChangeText={onChangeTextPassword}
+              />
+              <MaterialCommunityIcons
+                name={showPassword ? "eye-off" : "eye"}
+                size={24}
+                color="#aaa"
+                style={styles.toggle}
+                onPress={toggleShowPassword}
+              />
+            </View>
+            <Pressable onPress={"onPressFunction"} style={styles.button}>
+              <Text style={styles.buttonText}>Register</Text>
+            </Pressable>
+            <Pressable style={styles.signInButton}>
+              <Text style={styles.signInButtonText}>
+                Don't have an account? Sign up
+              </Text>
+            </Pressable>
           </View>
-          <Pressable onPress={"onPressFunction"} style={styles.button}>
-            <Text style={styles.buttonText}>Register</Text>
-          </Pressable>
-          <Pressable style={styles.signInButton}>
-            <Text style={styles.signInButtonText}>
-              Don't have an account? Sign up
-            </Text>
-          </Pressable>
-        </View>
+        </TouchableWithoutFeedback>
       </ImageBackground>
     </View>
   );
--- Screens/RegistrationScreen.jsx
+++ Screens/RegistrationScreen.jsx
@@ -1,13 +1,13 @@
 import { useEffect, useState } from "react";
-import { Alert, Button, ImageBackground, Pressable, StyleSheet, Text, TextInput, View } from "react-native";
+import { Alert, Button, ImageBackground, Keyboard, KeyboardAvoidingView, Platform, Pressable, StyleSheet, Text, TextInput, TouchableWithoutFeedback, View } from "react-native";
 import { MaterialCommunityIcons, Ionicons } from "@expo/vector-icons"; 
 import { styles } from "./styles";
 
 
   const RegistrationScreen = () => {
-  const [login, setLogin] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [login, onChangeTextLogin] = useState("");
+  const [email, onChangeTextEmail] = useState("");
+  const [password, onChangeTextPassword] = useState("");
   const [showPassword, setShowPassword] = useState(false);
 
   const toggleShowPassword = () => {
@@ -34,64 +34,69 @@ import { styles } from "./styles";
         source={require("../src/img/photo_bg.jpg")}
         style={styles.image}
       >
-        <View style={styles.registrationContainer}>
-          <View style={styles.photoContainer}>
-            <Ionicons
-              name="add-circle-outline"
-              size={25}
-              color="#FF6C00"
-              style={[styles.addIcon,{
-            transform: [{translateX: 12.5}],
-          },]}
-            />
-          </View>
-          <Text style={styles.titleText}>Registration</Text>
-          <TextInput
-            style={styles.input}
-            placeholder="Login"
-            type="login"
-            name="login"
-            value={login}
-            required
-            onChange={setLogin}
-          />
-          <TextInput
-            style={styles.emailInput}
-            placeholder="Email"
-            type="email"
-            name="email"
-            value={email}
-            required
-            onChange={setEmail}
-          />
-          <View style={styles.passwordContainer}>
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <View style={styles.registrationContainer}>
+            <View style={styles.photoContainer}>
+              <Ionicons
+                name="add-circle-outline"
+                size={25}
+                color="#FF6C00"
+                style={[
+                  styles.addIcon,
+                  {
+                    transform: [{ translateX: 12.5 }],
+                  },
+                ]}
+              />
+            </View>
+            <Text style={styles.titleText}>Registration</Text>
             <TextInput
               style={styles.input}
-              secureTextEntry={!showPassword}
-              placeholder="Password"
-              type="password"
-              name="password"
-              value={password}
+              placeholder="Login"
+              type="login"
+              name="login"
+              value={login}
               required
-              onChange={setPassword}
+              onChangeText={onChangeTextLogin}
             />
-            <MaterialCommunityIcons
-              name={showPassword ? "eye-off" : "eye"}
-              size={24}
-              color="#aaa"
-              style={styles.toggle}
-              onPress={toggleShowPassword}
+            <TextInput
+              style={styles.emailInput}
+              placeholder="Email"
+              type="email"
+              name="email"
+              value={email}
+              required
+              onChangeText={onChangeTextEmail}
             />
+            <View style={styles.passwordContainer}>
+              <TextInput
+                style={styles.input}
+                secureTextEntry={!showPassword}
+                placeholder="Password"
+                type="password"
+                name="password"
+                value={password}
+                required
+                onChangeText={onChangeTextPassword}
+              />
+              <MaterialCommunityIcons
+                name={showPassword ? "eye-off" : "eye"}
+                size={24}
+                color="#aaa"
+                style={styles.toggle}
+                onPress={toggleShowPassword}
+              />
+            </View>
+            <Pressable onPress={"onPressFunction"} style={styles.button}>
+              <Text style={styles.buttonText}>Register</Text>
+            </Pressable>
+            <Pressable style={styles.signInButton}>
+              <Text style={styles.signInButtonText}>
+                Already have an account? Sign in
+              </Text>
+            </Pressable>
           </View>
-          <Pressable onPress={"onPressFunction"} style={styles.button}>
-            <Text style={styles.buttonText}>Register</Text>
-          </Pressable>
-          <Pressable style={styles.signInButton}>
-            <Text style={styles.signInButtonText}>
-              Already have an account? Sign in
-            </Text>
-          </Pressable>
-        </View>
+        </TouchableWithoutFeedback>
       </ImageBackground>
     </View>
   );
